824000c5e58e2402f36eea930ddd0c75a679c818
sanchitbajpai <sanchitbajpai0511@gmail.com>
2026-01-11 23:07:56 +0530

    latest

--- script.js
+++ script.js
@@ -52,20 +52,26 @@
 //     localStorage.setItem("tasks", JSON.stringify(tasks));
 //   }
 // });
-const todoInput =document.getElementById("todo-input").addEventListener('click', ()=>{
-const taskText = todoInput.value.trim();
-if(taskText==="")
-  return;
-const newTask={
-  id:Date.now(),
-  task :taskText,
-  completed:false,
-};
-tasks.push(newTask);
-todoInput.value="";
+const todoInput = document.getElementById("todo-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let tasks = [];
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = todoInput.value.trim();
+  if (taskText === "") return;
+
+  const newTask = {
+    id: Date.now(),
+    task: taskText,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+  console.log(tasks);
+
+  todoInput.value = "";
 });
 
-document.getElementById("add-task-btn")
-document.getElementById("todo-list")
 
-let tasks=[];
